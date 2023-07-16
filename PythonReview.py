a16df2a6d6afcb478311c7af78a859d77ce0ea5b
boulosQ24-meet <boulosbqurbashi@gmail.com>
2023-07-16 22:47:03 +0300

    Create PythonReview.py

---
+++ PythonReview.py
@@ -0,0 +1,35 @@
+def create_youtube_video(title, description):
+	return {
+        'title': title,
+        'description': description,
+        'likes': 0,
+        'dislikes': 0,
+        'comments': {}
+    }
+def like (yt_video):
+	if 'likes' in yt_video:
+		yt_video['likes'] += 1
+		return yt_video
+
+def dislike(yt_video):
+    if 'dislikes' in yt_video:
+        yt_video['dislikes'] += 1
+    return yt_video
+
+def add_comment(yt_video, username, comment_text):
+    if 'comments' in yt_video:
+        yt_video['comments'][username] = comment_text
+    return yt_video
+
+new_video = create_youtube_video("Awesome Tutorial","test for code")
+
+print("Videos:", new_video)
+
+new_video = like(new_video)
+new_video = like(new_video)
+new_video = dislike(new_video)
+
+add_comment(new_video, "user1", "wooow")
+add_comment(new_video, "user2", "coool")
+print(new_video["comments"])
+
